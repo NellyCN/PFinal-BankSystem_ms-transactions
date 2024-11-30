5f30b754c812396c7c88aa1d039bff3aff32767c
NellyCN <njanetcumpa@gmail.com>
2024-11-30 00:38:21 -0500

    actualizando código con constructor

--- src/main/java/com/xyzbank/mstransactions/mstransactions/controller/TransactionController.java
+++ src/main/java/com/xyzbank/mstransactions/mstransactions/controller/TransactionController.java
@@ -3,21 +3,46 @@ package com.xyzbank.mstransactions.mstransactions.controller;
 import com.xyzbank.mstransactions.mstransactions.model.Transaction;
 import com.xyzbank.mstransactions.mstransactions.service.TransactionService;
 
-import lombok.RequiredArgsConstructor;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController             // Indica al framework que esta clase se usará para recibir peticiones HTTP
-@RequiredArgsConstructor
-@RequestMapping("/transactions")
+@RequestMapping("/transactions/")
 public class TransactionController {
 
     private final TransactionService transactionService;
 
-    @GetMapping
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/deposit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Transaction> deposit(@RequestBody Transaction transaction) {
+        return transactionService.deposit(transaction);
+    }
+
+    @PostMapping("/withdrawal")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Transaction> withdrawal(@RequestBody Transaction transaction) {
+        return transactionService.withdrawal(transaction);
+    }
+
+    @PostMapping("/transfer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Transaction> transfer(@RequestBody Transaction transaction) {
+        return transactionService.transfer(transaction);
+    }
+    @GetMapping("/history")
     public Flux<Transaction> getAllTransactions() {
         return transactionService.getAllTransactions(); // Llama al servicio para obtener todas las transacciones
     }
--- src/main/java/com/xyzbank/mstransactions/mstransactions/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/xyzbank/mstransactions/mstransactions/service/impl/TransactionServiceImpl.java
@@ -3,83 +3,77 @@ package com.xyzbank.mstransactions.mstransactions.service.impl;
 import com.xyzbank.mstransactions.mstransactions.model.Transaction;
 import com.xyzbank.mstransactions.mstransactions.repository.TransactionRepository;
 import com.xyzbank.mstransactions.mstransactions.service.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
-//@RequiredArgsConstructor
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
     // Implementación de métodos
-//    @Autowired
     private final TransactionRepository transactionRepository;
 
+    // Constructor para inyectar el repositorio
+    public TransactionServiceImpl(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    // DEPÓSITOS
     @Override
     public Mono<Transaction> deposit(Transaction transaction) {
         // Validar que el monto sea positivo.
         if (transaction.getAmount() <= 0) {
             return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "El monto debe ser positivo"));
         }
-
         // Crear la transacción de depósito.
         transaction.setType("Deposit");
         transaction.setDate(LocalDateTime.now());
-
         return transactionRepository.save(transaction);
     }
 
+    // RETIROS
     @Override
     public Mono<Transaction> withdrawal(Transaction transaction) {
         // Validar que el monto sea positivo.
         if (transaction.getAmount() <= 0) {
             return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "El monto debe ser positivo"));
         }
-
         // Simulo validación de saldo
-        Double currentBalance = 1000.0; // Saldo fijo
-        if (currentBalance < transaction.getAmount()) {
-            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Saldo insuficiente"));
-        }
-
-    // Crear la transacción de retiro.
+//        Double currentBalance = 1000.0; // Saldo fijo
+//        if (currentBalance < transaction.getAmount()) {
+//            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Saldo insuficiente"));
+//        }
+        // Crear la transacción de retiro.
         transaction.setType("Withdrawal");
         transaction.setDate(LocalDateTime.now());
-
         return transactionRepository.save(transaction);
     }
 
+    // TRANSFERENCIAS
     @Override
     public Mono<Transaction> transfer(Transaction transaction) {
         // Valido datos requeridos.
         if (transaction.getAmount() <= 0 || transaction.getDestinationAccount() == null) {
             return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datos inválidos para la transferencia"));
         }
-
-        // Simulo validación de saldo.
-        Double currentBalance = 1000.0; // Saldo Fijo
-        if (currentBalance < transaction.getAmount()) {
-            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Saldo insuficiente"));
-        }
-
+        // Simulo validación de saldo insuficiente.
+//        Double currentBalance = 1000.0; // Saldo Fijo
+//        if (currentBalance < transaction.getAmount()) {
+//            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Saldo insuficiente"));
+//        }
         // Crear la transacción de transferencia.
         transaction.setType("Transfer");
         transaction.setDate(LocalDateTime.now());
-
         return transactionRepository.save(transaction);
     }
 
-    // Constructor para inyectar el repositorio
-    @Autowired
-    public TransactionServiceImpl(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
-    }
-
+    // HISTORIAL DE TRANSACCIONES
     @Override
     public Flux<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
